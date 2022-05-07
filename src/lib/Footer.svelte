b5076b31f19ce8e8a2950b2e4df1924f06128b3e
RAIL_HU <52292794+RAIL-HU@users.noreply.github.com>
2022-05-07 16:22:31 -0700

    updated icons

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,6 @@
 <footer>
-    <h1>footer from html</h1>
+    <h1>Back to top</h1>
+    <h2>&copy; 2022</h2>
     <i class="fa-solid fa-book fa-2x"></i>
     <i class="fa-solid fa-book-bookmark fa-2x"></i>
     <i class="fa-solid fa-house-chimney fa-2x"></i>
@@ -8,6 +9,10 @@
     <i class="fa-solid fa-display fa-2x"></i>
     <i class="fa-solid fa-barcode fa-2x"></i>
     <i class="fa-solid fa-qrcode fa-2x"></i>
+    <i class="fa-solid fa-book-open fa-2x"></i>
+    <i class="fa-solid fa-user fa-2x"></i>
+    <i class="fa-solid fa-sliders fa-2x"></i>
+    <i class="fa-solid fa-arrow-right-from-bracket fa-2x"></i>
 </footer>
 
 <!-- <template lang="pug">
