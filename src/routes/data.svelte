f9f9149ec5ea0f248483044c5d52bb7fa0352fbd
Senti <senti@yatta.com>
2022-05-06 21:41:30 -0700

    Added data page

---
+++ src/routes/data.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let objects = [{ name: 'dummy object', description: 'very cool' }]
+</script>
+
+<template lang="pug">
+  p list of objects: {JSON.stringify(objects)}
+  +each('objects as object')
+    h1 Name of Object: 
+    p {object.name}
+    h2 Description of Object: 
+    p {object.description}
+</template>
