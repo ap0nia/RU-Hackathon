5a511c5f51def731ace206b5703e7c6a47740bf3
Senti <senti@yatta.com>
2022-05-07 11:13:43 -0700

    oopsies

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-  async function auth() {
-    await fetch('/api/authService', { method: 'GET' })
-  }
 </script>
 
 <template lang="pug">
-  form
-    input Username
-    input Password
+  h1 login page
 </template>
