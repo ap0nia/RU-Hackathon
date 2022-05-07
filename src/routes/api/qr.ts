e87ccabb53b54f8da86c12a9c442de9adc17e0a6
Senti <senti@yatta.com>
2022-05-07 09:49:50 -0700

    nice

---
+++ src/routes/api/qr.ts
@@ -0,0 +1,46 @@
+import { Openscreen } from '@openscreen/sdk'
+const os = new Openscreen().config({
+  key: import.meta.env.VITE_OS_API_KEY,
+  secret: import.meta.env.VITE_OS_API_SECRET,
+})
+const projectId = import.meta.env.VITE_PROJECT_ID
+
+async function main() {
+  // Create an asset and generate a QR Code
+  const asset = await os
+    .project(projectId)
+    .assets()
+    .create({
+      name: 'Hello World',
+      qrCodes: [
+        {
+          intent: 'https://openscreen.com/',
+          intentType: 'DYNAMIC_REDIRECT',
+        },
+      ],
+    })
+
+  // Returns the asset object you just created
+  console.log('Asset:', JSON.stringify(asset, '', 2))
+
+  // Note: The qrCodeId within the asset object can be accessed through asset.asset.qrCodes[0].qrCodeId
+  const { qrCodeId } = asset.asset.qrCodes[0]
+
+  // Returns a scannable QR Code and saves the png file in your project folder
+  const qrCode = await os.qrCode(qrCodeId).get({ format: 'png', dataUrl: true })
+  await os.saveQrImageDataToFile(qrCode, 'my-first-qr-code.png')
+
+  // Returns the Openscreen qrCode object
+  console.log('QR Code:', JSON.stringify(qrCode, '', 2))
+  return { asset, qrCode }
+}
+
+export async function get() {
+  const data = await main()
+
+  return {
+    body: {
+      data,
+    },
+  }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,53 @@
 <script lang="ts">
+  import fs from 'fs'
+
+  async function getQrCode() {
+    const res = await fetch('/api/qr')
+    const {
+      data: {
+        qrCode: {
+          image: { data: imageData },
+        },
+      },
+    } = await res.json()
+    localStorage.setItem('qr', imageData)
+  }
+
+  async function saveQrCode() {
+    // create a new handle
+    const saveOptions = {
+      types: [
+        {
+          description: 'image',
+          accept: { 'image/png': ['.png'] },
+        },
+      ],
+    }
+
+    const image = localStorage.getItem('qr')
+
+    const imageBlob = new Blob([atob(image)], { type: 'image/png' })
+    console.log(imageBlob)
+
+    const newHandle = await window.showSaveFilePicker(saveOptions)
+
+    const writeOptions = {}
+
+    // create a FileSystemWritableFileStream to write to
+    const writableStream = await newHandle.createWritable(writeOptions)
+
+    // write our file
+    await writableStream.write(atob(image))
+
+    // close the file and write the contents to disk.
+    await writableStream.close()
+  }
 </script>
 
 <template lang="pug">
   h1 hello, RU hacks!
+  button(on:click|preventDefault='{getQrCode}') Get QR Code
+  button(on:click|preventDefault='{saveQrCode}') Save the QR Code
 </template>
 
 <style lang="stylus">
