d19fea32f9bd7c6470011d9db88249ac5f4b8385
Eric Pedley <ericpedley@gmail.com>
2022-05-07 00:38:47 -0700

    got scan to form behavior working correctly

--- src/routes/form.svelte
+++ src/routes/form.svelte
@@ -1,8 +1,20 @@
 <script lang="ts" context="module">
   export async function load({url}) {
+    const assetID = url.searchParams.get('id')
+    let didUpdate=false;
+    if(url.searchParams.get('name')) {
+      didUpdate=true;
+      const customAttributes = {
+        name: url.searchParams.get('name'),
+        description: url.searchParams.get('description')
+      }
+      console.log(assetID, customAttributes)
+      //use openscreen to update asset with new custom attributes
+    }
     return {
       props: {
-        id: url.searchParams.get('id')
+        assetID,
+        didUpdate
       }
     }
   }
@@ -15,17 +27,22 @@
 </script>
 
 <script lang="ts">
-  export let id
+  export let assetID
+  export let didUpdate
 </script>
 
 <template lang="pug">
+  p {didUpdate}
+  //only render the following if didUpdate is true
+  p Success! You have registered the asset.
   form
     label(for='name') Name of Item
       input(type='text' name='name' bind:value='{name}')
     label(for='description') Description
-      input(type='text' name='name' bind:value='{description}')
+      input(type='text' name='description' bind:value='{description}')
+    input(type='hidden' name='id' value='{assetID}')
     button(type='submit' on:click='{submit}') Submit
-  p {id}
+  p {assetID}
   h1 current form state:
   p name: {name}
   p description: {description}
