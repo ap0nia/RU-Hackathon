b9c72239f24f02ba8739d1d465b1524e04ccb66b
Senti <senti@yatta.com>
2022-05-07 12:17:26 -0700

    added auth

---
+++ src/lib/auth_config.ts
@@ -0,0 +1,6 @@
+const config = {
+    domain: "dev-7cuth4w7.us.auth0.com",
+    clientId: "a962o21ab00BJAWQGhT59d1hsFrPTnmC"
+};
+  
+export default config;
--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,3 +1,17 @@
+<script lang="ts" context="module">
+  export async function load({ session }) {
+    return {
+      props: {
+        username: session.username,
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let username
+</script>
+
 <template lang="pug">
   h1 hello {username}
 </template>
