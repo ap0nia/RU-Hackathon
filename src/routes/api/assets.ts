22f70b272f05e8b97605d8e074e804e387510925
Eric Pedley <ericpedley@gmail.com>
2022-05-07 18:15:48 -0700

    merged bvo_style branch

--- src/routes/api/assets.ts
+++
@@ -1,9 +0,0 @@
-import {os, projectId} from '$lib/openscreen'
-export async function get() {
-    const assets = await os.project(projectId).assets().get();
-    return {
-      body: {
-        assets,
-      },
-    }
-}
---
+++ src/routes/profile/codeListing.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import QR_Card from '$lib/QR_Card.svelte'
+  export let assets
+</script>
+
+<template lang="pug">
+  +each('assets as asset')
+    QR_Card(asset='{asset}')
+</template>
