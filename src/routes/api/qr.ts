a818996bd5e25ed3e41b5d68ab8fd4d70b2997bd
Eric Pedley <ericpedley@gmail.com>
2022-05-07 19:04:49 -0700

    made qr codes redirect to our site and have scan id

--- src/routes/api/qr.ts
+++ src/routes/api/qr.ts
@@ -6,17 +6,18 @@ async function main() {
     .project(projectId)
     .assets()
     .create({
-      name: 'Hello World',
+      name: 'Generated Asset',
       qrCodes: [
         {
-          intent: 'https://openscreen.com/',
+          intent: 'https://ru-hackathon.vercel.app/scan',
           intentType: 'DYNAMIC_REDIRECT',
+          dynamicRedirectType: 'SCAN_ID_IN_QUERY_STRING_PARAMETER'
         },
       ],
     })
 
   // Returns the asset object you just created
-  console.log('Asset:', JSON.stringify(asset, '', 2))
+  // console.log('Asset:', JSON.stringify(asset, '', 2))
 
   // Note: The qrCodeId within the asset object can be accessed through asset.asset.qrCodes[0].qrCodeId
   const { qrCodeId } = asset.asset.qrCodes[0]
@@ -26,7 +27,7 @@ async function main() {
   await os.saveQrImageDataToFile(qrCode, 'my-first-qr-code.png')
 
   // Returns the Openscreen qrCode object
-  console.log('QR Code:', JSON.stringify(qrCode, '', 2))
+  // console.log('QR Code:', JSON.stringify(qrCode, '', 2))
   return { asset, qrCode }
 }
 
